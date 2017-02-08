3de917c18897ad404ff6ca8f9d53e4e27830e163
Computer Science <math@rkc100-09.bard.edu>
2017-02-08 16:57:17 -0500

    commiting in class work to github

---
+++ empty-example/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(480,120);
+}
+
+function draw() {
+  background(204);
+    beginShape();
+    vertex(50, 120);
+    vertex(100, 90);
+    endShape();
+    fill(0);
+
+}
+
